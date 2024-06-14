84fe3842115abe512feb9895fd3352fe308d9c1c
LatifovMUhammadjon <latifov7864@gmail.com>
2024-06-14 11:27:15 +0500

    updated

--- expenses/admin.py
+++ expenses/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Expense, Currency
+from .models import Expense, Currency, ExchangeRate
 
 admin.site.register(Expense)
 admin.site.register(Currency)
+admin.site.register(ExchangeRate)
